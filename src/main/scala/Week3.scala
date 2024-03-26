e561e28eea40698fa1c17b1ff78652d597fcd83c
olwen.pearce <olwen.pearce@ovoenergy.com>
2024-03-26 11:32:38 +0000

    Week 3 WIP

---
+++ src/main/scala/Week3.scala
@@ -0,0 +1,25 @@
+import java.lang.Character.isDigit
+
+object Week3 {
+ def stringToPartNumbers(input: List[String]): List[Int] = {
+   //find any number adjacent to a symbol
+   //even diagonally etc
+
+   //So want the indexs of anything that's a number
+   //maybe enum type for digit, symbol etc and have this represent all
+   val numbersToCheck = input.map(string => string.map(char => isDigit(char)))
+
+   //I think can just run the check for every digit, then later bunch to numbers
+   //If we get true for any digit, number is true
+
+   val hasAjacent = numbersToCheck.map((row, rowIndex) => row.map((item, columnIndex) => (item == true &  input.indexOf(columnIndex).indexOf(columnIndex - 1))))
+
+ }
+
+  def hasAjacent(columnIndex: Int, rowIndex: Int, input: List[String]): Boolean = {
+    //check every item in the 6x6 square for a symbol
+    //don't need to filter out the centre as it's a number we know it's not a sumbol
+
+
+  }
+}
